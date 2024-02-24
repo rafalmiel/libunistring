2a6b2a826c408eaeca3038156d1f512a64b560bd
Bruno Haible <bruno@clisp.org>
2024-02-24 11:03:15 +0100

    Prepare for 1.2 release.
    * version.sh: Bump version number, date, gnulib version.
    * README: Update version.
    * lib/Makefile.am (LTV_*): Update to 6:0:1 (determined with help from
    gnulib's libtool-next-version).

--- version.sh
+++ version.sh
@@ -1,6 +1,6 @@
 # Version number and release date.
-VERSION_NUMBER=1.1
-RELEASE_DATE=2022-10-16      # in "date +%Y-%m-%d" format
+VERSION_NUMBER=1.2
+RELEASE_DATE=2024-02-24      # in "date +%Y-%m-%d" format
 
 # Version of gnulib that was used in this release.
-GNULIB_GIT_COMMIT=2118e7cf12997850652002b3af3c44511c98f4bc
+GNULIB_GIT_COMMIT=5828723e285945975dbb3d2e89cd8854d5b7a075
